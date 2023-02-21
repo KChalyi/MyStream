8dc76d8686a043fb4589514763f4fd646fe7c2a4
KChalyi <125888138+KChalyi@users.noreply.github.com>
2023-02-21 09:53:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-@st.cache_data(ttl=600)
 st.title("A Simple Streamlit Web App")
 name=st.text_input("Enter your name",' ')
 st.write(f"Hello {name}!")
@@ -9,6 +8,7 @@ x=st.slider("Select an integer x",0,10,1)
 y=st.slider("Select an integer y",0,10,1)
 df=pd.DataFrame({"x":[x],"y":[y],"x+y":[x+y]},index=["addition row"])
 st.write(df)
+@st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
