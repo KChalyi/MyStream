5d8cad8b7230adff7462b11324a1da0f21f5e327
KChalyi <125888138+KChalyi@users.noreply.github.com>
2023-02-21 14:19:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("A Simple Streamlit Web App")
-name=st.text_input("Enter your name",' ')
-st.write(f"Hello {name}!")
-x=st.slider("Select an integer x",0,10,1)
-y=st.slider("Select an integer y",0,10,1)
-df=pd.DataFrame({"x":[x],"y":[y],"x+y":[x+y]},index=["addition row"])
-st.write(df)
-#@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
-df2 = load_data(st.secrets["public_gsheets_url"])
-#col_names=["Col1","Col2"]
-#df2.columns=col_names
-#row_names=["R1","R2"]
-#df2.index=row_names
+st.title("Расчёт падения напряжения")
 st.write(df2)
 
 # CSS to inject contained in a string
