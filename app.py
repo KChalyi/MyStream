5ee0ea73bcb0f748b6230804423b3e5b0383cd79
KChalyi <125888138+KChalyi@users.noreply.github.com>
2023-02-21 11:14:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,17 @@ df2.columns=col_names
 row_names=["R1","R2"]
 df2.index=row_names
 st.write(df2)
+
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+# Display a static table
+st.table(df2)
